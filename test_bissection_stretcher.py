ddd9e9886156ec50ddd7d11d2ef63298823bf93c
Tiago Vargas <tiago.ce10@outlook.com>
2023-02-07 12:59:18 -0300

    sub-feat: stretch interval of constraint based on other the constraints
    given $x \in [0, 5]$ and $x \le 3$, stretch the constraint $x \le 0$ to $x \le 3$

--- test_bissection_stretcher.py
+++ test_bissection_stretcher.py
@@ -13,3 +13,14 @@ class TestStretcher:
             bissection_stretcher.stretch_to_the_right(model, constraint)
 
             assert constraint.right_expr.constant == 5
+
+        def test_base_on_another_constraint(self):
+            model = Model()
+            x = model.continuous_var(lb=0, ub=5, name='x')
+            model.add_constraint(x <= 3)
+            constraint = model.add_constraint(x <= 0, ctname='constraint to stretch')
+
+            bissection_stretcher.stretch_to_the_right(model, constraint)
+
+            assert constraint.right_expr.constant == 3
+
