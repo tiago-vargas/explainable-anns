5015c5428e6c012acb7e1e281356ebb46c5cc718
Tiago Vargas <tiago.ce10@outlook.com>
2023-04-28 19:51:50 -0300

    formulate 2+1 networks

--- milp_model_generator.py
+++ milp_model_generator.py
@@ -1,3 +1,4 @@
+import numpy as np
 from docplex.mp.model import Model
 from keras.models import Sequential
 
@@ -11,10 +12,14 @@ class MILPModel:
         layer = network.layers[0]
         weights = layer.weights[0].numpy()
         biases = layer.weights[1].numpy()
-        i = self._model.continuous_var(name='i')
+        input_size = network.input_shape[1]
+        i = self._model.continuous_var_list(keys=input_size, name='i')
         o = self._model.continuous_var(name='o')
         s = self._model.continuous_var(name='s')
-        self._model.add_constraint(weights[0, 0] * i + biases[0] == o - s)
+
+        i = np.array(i)
+        self._model.add_constraint(weights.T[0, :] @ i + biases[0] == o - s)
+
         z = self._model.binary_var(name='z')
         self._model.add_indicator(binary_var=z, active_value=1, linear_ct=(o <= 0))
         self._model.add_indicator(binary_var=z, active_value=0, linear_ct=(s <= 0))
--- test_milp_model_generator.py
+++ test_milp_model_generator.py
@@ -16,7 +16,23 @@ class TestFormulatingNetwork:
 
         strings = [x.to_string() for x in m.formulation]
         assert strings == [
-            '11i+50 == o-s',
+            '11i_0+50 == o-s',
+            'z -> [o <= 0]',
+            'z=0 -> [s <= 0]',
+        ]
+
+    def test_2x1_network(self):
+        nn = Sequential()
+        nn.add(Dense(units=1, input_dim=2))
+        weights = np.array([[11], [12]])
+        biases = np.array([50])
+        nn.layers[0].set_weights([weights, biases])
+
+        m = MILPModel(nn)
+
+        strings = [x.to_string() for x in m.formulation]
+        assert strings == [
+            '11i_0+12i_1+50 == o-s',
             'z -> [o <= 0]',
             'z=0 -> [s <= 0]',
         ]
