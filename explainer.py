eb038b6bbf9b43a12ecb2728e89d1b81480d9ce6
Tiago Vargas <tiago.ce10@outlook.com>
2023-05-16 14:30:30 -0300

    fake: get relevant features for single (relevant) feature

--- explainer.py
+++ explainer.py
@@ -1,9 +1,9 @@
-from docplex.mp.model import Model
+from milp_model_generator import MILPModel
 
 
 class Explainer:
-    def __init__(self, milp_model: Model, predicting_function, feature_labels: list[str]):
-        pass
+    def __init__(self, milp_model: MILPModel, predicting_function, feature_labels: list[str]):
+        self.feature_labels = feature_labels
 
     def get_relevant_features(self, input: list[float]) -> list[str]:
-        return ['A']
+        return self.feature_labels
